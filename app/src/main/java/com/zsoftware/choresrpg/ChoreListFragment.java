d84322cb59af206030e13b8c39f58512d4131ae5
Zygimantas Kadisa <kadisa917@gmail.com>
2023-01-25 23:33:46 +0200

    chore list ui

--- app/src/main/java/com/zsoftware/choresrpg/ChoreListFragment.java
+++ app/src/main/java/com/zsoftware/choresrpg/ChoreListFragment.java
@@ -1,37 +1,35 @@
 package com.zsoftware.choresrpg;
 
-import static android.content.Context.MODE_PRIVATE;
-
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.zsoftware.choresrpg.adapters.ChoreAdapter;
-import com.zsoftware.choresrpg.choreicon.ChoreIconDictionary;
-import com.zsoftware.choresrpg.choreicon.ChoreIconEnum;
 import com.zsoftware.choresrpg.databinding.FragmentChoreListBinding;
 
+import java.util.Calendar;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
 
 public class ChoreListFragment extends Fragment {
 
     private FragmentChoreListBinding binding;
+    private Dictionary<WeekDay, ListView> ChoreLists;
+    private Dictionary<WeekDay, ImageView> WeekImages;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
-
         binding = FragmentChoreListBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
@@ -40,21 +38,72 @@ public class ChoreListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.addChoreFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        ChoreLists = new Hashtable<WeekDay, ListView>();
+        ChoreLists.put(WeekDay.Monday, binding.mondayChoreListListView);
+        ChoreLists.put(WeekDay.Tuesday, binding.tuesdayChoreListListView);
+        ChoreLists.put(WeekDay.Wednesday, binding.wednesdayChoreListListView);
+        ChoreLists.put(WeekDay.Thursday, binding.thursdayChoreListListView);
+        ChoreLists.put(WeekDay.Friday, binding.fridayChoreListListView);
+        ChoreLists.put(WeekDay.Saturday, binding.saturdayChoreListListView);
+        ChoreLists.put(WeekDay.Sunday, binding.sundayChoreListListView);
+
+        WeekImages = new Hashtable<WeekDay, ImageView>();
+        WeekImages.put(WeekDay.Monday, binding.mondayImageView);
+        WeekImages.put(WeekDay.Tuesday, binding.tuesdayImageView);
+        WeekImages.put(WeekDay.Wednesday, binding.wednesdayImageView);
+        WeekImages.put(WeekDay.Thursday, binding.thursdayImageView);
+        WeekImages.put(WeekDay.Friday, binding.fridayImageView);
+        WeekImages.put(WeekDay.Saturday, binding.saturdayImageView);
+        WeekImages.put(WeekDay.Sunday, binding.sundayImageView);
+
+        for (WeekDay d: WeekDay.values())
+        {
+            WeekDay wd = d;
+            WeekImages.get(d).setOnClickListener(view12 -> UpdateChoreListViews(wd));
+        }
+
+        binding.addChoreFab.setOnClickListener(view1 ->
                 NavHostFragment.findNavController(ChoreListFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_addChoreFragment);
-            }
-        });
-        UpdateChoreListView();
+                .navigate(R.id.action_FirstFragment_to_addChoreFragment));
+        UpdateChoreListViews(GetCurrentWeekDay());
     }
 
-    public void UpdateChoreListView()
+    public void UpdateChoreListViews(WeekDay selectedWeek)
     {
         List<Chore> chores = SaveData.Instance().GetChoreList();
-        ChoreAdapter arrayAdapter =  new ChoreAdapter(getContext(), chores, WeekDay.Monday);
-        binding.choreListListView.setAdapter(arrayAdapter);
+        for (WeekDay d: WeekDay.values())
+        {
+            ChoreAdapter arrayAdapter =  new ChoreAdapter(getContext(), chores, d);
+            ListView lv = ChoreLists.get(d);
+            lv.setAdapter(arrayAdapter);
+            lv.setVisibility(View.GONE);
+
+            ImageView iv = WeekImages.get(d);
+            iv.setImageDrawable(getResources().getDrawable(R.drawable.rounded_box));
+        }
+
+        ListView currentDayView = ChoreLists.get(selectedWeek);
+        currentDayView.setVisibility(View.VISIBLE);
+        ImageView currentImageView = WeekImages.get(selectedWeek);
+        currentImageView.setImageDrawable(getResources().getDrawable(R.drawable.rounded_box_selected));
+    }
+
+    private WeekDay GetCurrentWeekDay()
+    {
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+
+        switch (day)
+        {
+            case Calendar.MONDAY: return WeekDay.Monday;
+            case Calendar.TUESDAY: return WeekDay.Tuesday;
+            case Calendar.WEDNESDAY: return WeekDay.Wednesday;
+            case Calendar.THURSDAY: return WeekDay.Thursday;
+            case Calendar.FRIDAY: return WeekDay.Friday;
+            case Calendar.SATURDAY: return WeekDay.Saturday;
+            case Calendar.SUNDAY: return WeekDay.Sunday;
+        }
+        return null;
     }
 
     @Override
@@ -66,6 +115,6 @@ public class ChoreListFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        UpdateChoreListView();
+        UpdateChoreListViews(GetCurrentWeekDay());
     }
 }
